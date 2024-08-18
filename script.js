07d0b47e09d07f9e0f8ee62579ce9707f2539337
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-08-18 07:04:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function selectType(type, iconSrc) {
     selectedIconSrc = iconSrc; // Spara den valda ikonens källa
     document.getElementById('categoryInput').value = type;
     document.getElementById('formTitle').innerText = 'Lägg till ' + type;
+    document.getElementById('formIcon').src = iconSrc; // Sätt ikonen i formulärets rubrik
     document.getElementById('startMessage').style.display = 'none';
     document.getElementById('nameInput').placeholder = 'Namn på ' + type.toLowerCase();
     document.getElementById('urlInput').placeholder = type + ' hemsida/facebook';
