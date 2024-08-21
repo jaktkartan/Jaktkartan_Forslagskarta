3bc08d5f2c02f2e7c56d1646ceebdb437e727345
jaktkartan <167991082+jaktkartan@users.noreply.github.com>
2024-08-22 00:15:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -278,6 +278,56 @@ function hideAllMenus() {
     document.getElementById('thankYouMessage').style.display = 'none';
 }
 
+// Ny funktion för att ladda GeoJSON-data och visa den på kartan
+function loadGeoJSONData() {
+    // URLs till GeoJSON-filerna
+    var layerURLs = {
+        'Mässor': ['bilder/Massor.geojson'],
+        'Jaktkort': ['bilder/jaktkort.geojson'],
+        'Jaktskyttebanor': ['bilder/jaktskyttebanor.geojson']
+    };
+
+    // Stil för markörer
+    var layerStyles = {
+        'Mässor': {
+            iconUrl: 'bilder/ikon_massor.png',
+            iconSize: [40, 40]
+        },
+        'Jaktkort': {
+            iconUrl: 'bilder/ikon_jaktkort.png',
+            iconSize: [40, 40]
+        },
+        'Jaktskyttebanor': {
+            iconUrl: 'bilder/ikon_jaktskyttebanor.png',
+            iconSize: [40, 40]
+        }
+    };
+
+    Object.keys(layerURLs).forEach(function(layerName) {
+        layerURLs[layerName].forEach(function(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    var layer = L.geoJSON(data, {
+                        pointToLayer: function(feature, latlng) {
+                            var icon = L.icon({
+                                iconUrl: layerStyles[layerName].iconUrl,
+                                iconSize: layerStyles[layerName].iconSize
+                            });
+                            return L.marker(latlng, { icon: icon });
+                        },
+                        onEachFeature: function(feature, layer) {
+                            layer.on('click', function() {
+                                alert(`Du klickade på ett ${layerName.toLowerCase()}. Mer funktionalitet kommer snart.`);
+                            });
+                        }
+                    }).addTo(map);
+                })
+                .catch(error => console.error('Error loading GeoJSON:', error));
+        });
+    });
+}
+
 document.getElementById('suggestionForm').onsubmit = function(event) {
     event.preventDefault();
 
